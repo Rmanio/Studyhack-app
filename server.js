487234879332004d90947e3f3a20a496678f61a3
Daniar12 <98156588+Daniar12@users.noreply.github.com>
2022-05-09 23:48:05 +0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/',function(req,res) {
+    res.sendfile(path.join(__dirname + '/public/view/index.html'));
+});
+
+app.get('/education',function(req,res) {
+    res.sendfile(path.join(__dirname + '/public/view/education.html'));
+});
+
+app.get('/timer',function(req,res) {
+    res.sendfile(path.join(__dirname + '/public/view/Timer.html'));
+});
+
+app.get('/calculato',function(req,res) {
+    res.sendfile(path.join(__dirname + '/public/view/MathCalculator.html'));
+});
+
+app.get('/materials',function(req,res) {
+    res.sendfile(path.join(__dirname + '/public/view/Materials.html'));
+});
+
+app.get('/news',function(req,res) {
+    res.sendfile(path.join(__dirname + '/public/view/index1.html'));
+});
+
+app.get('/schedule',function(req,res) {
+    res.sendfile(path.join(__dirname + '/public/view/Event%20Timer.html'));
+});
+
+app.get('/register',function(req,res) {
+    res.sendfile(path.join(__dirname + '/public/view/iudex.html'));
+});
+
+app.get('/profile',function(req,res) {
+    res.sendfile(path.join(__dirname + '/public/view/profilepage.html'));
+});
+
+app.listen(process.env.port || 3000);
+
+console.log('Running at http://localhost:3000');
